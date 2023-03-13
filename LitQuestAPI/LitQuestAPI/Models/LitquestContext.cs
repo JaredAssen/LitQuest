8c49a07a35e9aa6bc8a24371ab327836ac4f856f
JaredAssen <80238577+JaredAssen@users.noreply.github.com>
2023-03-13 12:35:01 -0600

    controller work

--- LitQuestAPI/LitQuestAPI/Models/LitquestContext.cs
+++ LitQuestAPI/LitQuestAPI/Models/LitquestContext.cs
@@ -30,7 +30,7 @@ public partial class LitquestContext : DbContext
         //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         if (!optionsBuilder.IsConfigured)
         {
-            optionsBuilder.UseMySQL("server=127.0.0.1;uid=root;pwd=____;database=litquest");
+            optionsBuilder.UseMySQL("DevConnection");
         }
     }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
