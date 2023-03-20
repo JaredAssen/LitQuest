7d828fb911eb63395b9be0dfdb03b4c15bf3f8fd
shreosidebnath <101438457+shreosidebnath@users.noreply.github.com>
2023-03-20 15:51:26 -0600

    login and register page

--- view/src/pages/LoginPage/LoginPage.js
+++ view/src/pages/LoginPage/LoginPage.js
@@ -1,11 +1,34 @@
-import React from 'react'
+
+import './login.css';
+import React, { useState } from 'react';
 
 const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  
+  const login = () => {
+    if (username === 'admin' && password === '123') {
+      alert('Login successful!');
+    } else {
+      alert('Incorrect username or password.');
+    }
+  };
+  
   return (
     <div>
-      Login
+      <h1>Login Page</h1>
+      <form>
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} /><br />
+        
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} /><br />
+        
+        <input type="button" value="Login" onClick={login} />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
+
