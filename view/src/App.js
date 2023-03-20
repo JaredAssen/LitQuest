9d0812845edc0dad01412b9edfd7038bbc0de4aa
zohaib2001 <102832419+zohaib2001@users.noreply.github.com>
2023-03-20 15:38:07 -0600

    profilepage
    uploaded profilepage src instead of merge

---
+++ view/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Profile from './Profile';
+
+
+function App() {
+  return (
+    <div>
+        <Profile /> 
+      </div>
+  );
+}
+
+export default App;
--- view/src/index.js
+++ view/src/index.js
@@ -1,34 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {
-  BrowserRouter, Routes, Route
-} from 'react-router-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import HomePage from './pages/HomePage/HomePage';
-import RegisterPage from './pages/RegisterPage/RegisterPage';
-import BookList from './components/BookList/BookList';
-import BookDetails from './components/BookDetails/BookDetails'
-import LoginPage from './pages/LoginPage/LoginPage';
-import { AppProvider } from './context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <AppProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path = "/" element = {<HomePage />}>
-          <Route path = "register" element = {<RegisterPage />}/>
-          <Route path = "login" element = {<LoginPage />}/>
-          <Route path = "book" element = {<BookList/>}/>
-          <Route path = "/book/:id" element = {<BookDetails />}/>
-      
-        </Route>
-      </Routes>
-
-    </BrowserRouter>
-
-  </AppProvider>
-
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
