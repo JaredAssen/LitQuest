996298e857eef0d20ac8d57ae9a7df8a496e7aef
WillPerks <94204768+WillPerks@users.noreply.github.com>
2023-03-20 19:36:33 -0600

    Make username unique

--- DatabaseDump/litquest_user.sql
+++ DatabaseDump/litquest_user.sql
@@ -27,7 +27,8 @@ CREATE TABLE `user` (
   `password` varchar(45) NOT NULL,
   `email` varchar(45) NOT NULL,
   `userid` int NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`userid`)
+  PRIMARY KEY (`userid`),
+  UNIQUE(`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
